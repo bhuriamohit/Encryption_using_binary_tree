ce5a9bd4d5c96761d2da89ac19c1bf23d48d254f
Mohit <2021csb1109@iitrpr.ac.in>
2022-11-07 17:46:55 +0530

    comment

--- main.c
+++ main.c
@@ -1,8 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t len = 0;
+typedef struct Node Node;
 
+// Define the Tree Node here
+struct Node {
+    int value;
+    // Pointers to the left and right children
+    Node* left, *right;
+};
+
+
+Node* init_tree(int data) {
+    // Creates the tree and returns the
+    // root node
+    Node* root = (Node*) malloc (sizeof(Node));
+    root->left = root->right = NULL;
+    root->value = data;
+    return root;
+}
+
+Node* create_node(int data) {
+    // Creates a new node
+    Node* node = (Node*) malloc (sizeof(Node));
+    node->value = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void free_tree(Node* root) {
+    // Deallocates memory corresponding
+    // to every node in the tree.
+    Node* temp = root;
+    if (!temp)
+        return;
+    free_tree(temp->left);
+    free_tree(temp->right);
+    if (!temp->left && !temp->right) {
+        free(temp);
+        return;
+    }
+}
+
+int tree_height(Node* root) {
+    // Get the height of the tree
+    if (!root)
+        return 0;
+    else {
+        // Find the height of both subtrees
+        // and use the larger one
+        int left_height = tree_height(root->left);
+        int right_height = tree_height(root->right);
+        if (left_height >= right_height)
+            return left_height + 1;
+        else
+            return right_height + 1;
+    }
+}
+
+void print_level(Node* root, int level_no) {
+    // Prints the nodes in the tree
+    // having a level = level_no
+    
+    // We have a auxiliary root node
+    // for printing the root of every
+    // subtree
+    if (!root)
+        return;
+    if (level_no == 0) {
+        // We are at the top of a subtree
+        // So print the auxiliary root node
+        printf("%d -> ", root->value);
+    }
+    else {
+        // Make the auxiliary root node to
+        // be the left and right nodes for
+        // the subtrees and decrease level by 1, since
+        // you are moving from top to bottom
+        print_level(root->left, level_no - 1);
+        print_level(root->right, level_no - 1);
+    }
+
+}
+
+void print_tree_level_order(Node* root) {
+    if (!root)
+        return;
+    int height = tree_height(root);
+    for (int i=0; i<height; i++) {
+        printf("Level %d: ", i);
+        print_level(root, i);
+        printf("\n");
+    }
+    printf("\n\n-----Complete Level Order Traversal:-----\n");
+    for (int i=0; i<height; i++) {
+        print_level(root, i);
+    }
+    printf("\n");
+}
+
+//to track the length of message
+size_t len = 0;
 
 char *inputString(FILE *fp, size_t size)
 {
@@ -12,34 +110,45 @@ char *inputString(FILE *fp, size_t size)
     str = realloc(NULL, sizeof(*str) * size); // size is start size
     if (!str)
         return str;
+        //getting the pointer to the end of file
     while (EOF != (ch = fgetc(fp)) && ch != '\n')
     {
         str[len++] = ch;
         if (len == size)
-        {
+        {//if size is full 
+        //increasing size
             str = realloc(str, sizeof(*str) * (size += 16));
             if (!str)
                 return str;
         }
-    }
+    }//ending the string with
     str[len++] = '\0';
-
-    return realloc(str, sizeof(*str) * len);
+//removing the extra space asigned
+    return realloc(str, sizeof(*str) * len); 
 }
+//function for encryption
 void encrypt(void)
 {
     char *m;
     printf("\nInput the message you want to encrypt: \n");
     scanf("\n");
+    //calling the input function
     m = inputString(stdin, 10);
-    // printf("%s\n", m);
-    //  printf("%d",len);
+   
+    //making the length even for proper pairing
     if (len % 2 != 0)
     {
         len--;
     }
+    //declaring array of lenght to keep order of ascii value
     int a[len + 1];
     a[0] = 0;
+    /*
+    First level decryption
+    step 1:Convert the characters of the intermediate message into corresponding ASCII values
+    Step 2:Form pairs with the ASCII values.
+    Step 3:Interchange  the ASCII values.
+    */
     for (int i = 0; i < len;)
     {
         int temp = (int)m[i] + i + 2;
@@ -49,11 +158,9 @@ void encrypt(void)
         a[i + 1] = temp;
         i++;
     }
+    //free the pointer as we have array of ascii
     free(m);
-    // for (int i = 1; i <= len; i++)
-    // {
-    //     printf("%c", a[i]);
-    // }
+ //performing the level order traversal of binary tree
     for (int i = 2; i <= len;)
     {
         if (i + 1 <= len)
@@ -66,22 +173,26 @@ void encrypt(void)
         i = i + 2;
     }
     printf("\nyOUR encrypted message is:\n");
+    //printing the encrypted msg
     for (int i = 1; i <= len; i++)
     {
         printf("%c", a[i]);
     }
-}
+}//function for decryption
 void decrypt(void)
 {
     char *m;
 
     printf("\nInput the message you want to decrypt:\n ");
     scanf("\n");
+    //taking string input
     m = inputString(stdin, 10);
-
+// removing the last character
     len--;
+    //declaring array of length
     int a[len + 1];
     a[0] = 0;
+    //substracting the index as per the paper
     for (int i = 0; i < len;)
     {
         int temp = (int)m[i];
@@ -91,11 +202,12 @@ void decrypt(void)
         a[i + 1] = temp;
         i++;
     }
+    //freeing the pointer
     free(m);
     //   printf("\n");
-
+//performing the level orcer traversal of complete binary tree
     for (int i = 2; i <= len;)
-    {
+    {//we can perform the traversal only if we have the next element
         if (i + 1 <= len)
         {
             int d = a[i];
@@ -103,12 +215,11 @@ void decrypt(void)
 
             a[i + 1] = d;
         }
+        //jump 2 index ahead
         i = i + 2;
     }
-    //  for (int i = 1; i <= len; i++)
-    // {
-    //     printf("%c", a[i]);
-    // }
+   
+    //substracting the index pf paired characters
     for (int i = 1; i <= len;)
     {
         int temp = a[i];
@@ -128,12 +239,13 @@ void decrypt(void)
 int main(void)
 {
     
-
-    
     char c;
     printf("Welcome to Bhuria Security Agency\n");
+
         printf("\n1. To encrypt\n2. to decrypt\n3. quit");
+
         printf("\nEnter the choice(1 or 2 or 3)\n");
+
         scanf("\n%c", &c);
         
         if (c == '1')
